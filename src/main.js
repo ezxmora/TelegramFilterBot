3cc36dd2f0b9f98d5f527bdeafbbbd0e6600d457
Ezxmora <erik1997@hotmail.es>
2024-03-03 23:43:27 +0000

    Fixed issue with text and cleaned up

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { Telegraf } from "telegraf";
+import { Telegraf, Telegram } from "telegraf";
 import fs from "fs";
 import "dotenv/config";
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN, { username: "McFarlaneBot" });
 let blacklist;
 
 const validateUrl = (url) => {
@@ -17,28 +17,38 @@ const validateUrl = (url) => {
 };
 
 bot.on("message", async (ctx) => {
-  const messageContent = ctx.update.message?.text;
-  if (!messageContent.length) return;
+  const { from, text, message_id, chat } = ctx.message;
+  if (!text?.length) return;
 
+  const splittedMessage = text.split(" ");
   const findUrl = splittedMessage.find((v) => v.includes("https://"));
   if (findUrl) {
     const validUrl = validateUrl(findUrl.toString());
     if (validUrl) {
-      const { id, username, first_name } = ctx.message.from;
-      process.stdout.write(
-        `DELETED ${id} - ${username} / ${first_name} - ${findUrl}\n`
-      );
-      await ctx.reply(
-        `¡Vaya ${first_name}! 😱\nParece que has pasado un tweet de un subnormal registrado.\nSi crees que ha sido un falso positivo contacta con el administrador del bot.`
-      );
-      await ctx.deleteMessage(ctx.message.message_id);
+      const { username, first_name } = from;
+
+      try {
+        process.stdout.write(
+          `DELETED ${message_id} - @${username} / ${first_name} - ${findUrl}\n`
+        );
+        await ctx.telegram.sendMessage(
+          chat.id,
+          `¡Vaya ${first_name}! 😱\nParece que has pasado un tweet de un subnormal registrado.\nSi crees que ha sido un falso positivo contacta con el administrador del bot.`
+        );
+        await ctx.deleteMessage(ctx.update.message.message_id);
+      } catch (e) {
+        console.error(e);
+      }
     }
   }
 });
 
-bot.launch(() => {
+bot.launch({ allowedUpdates: true }, () => {
   blacklist = fs
     .readFileSync(`${process.cwd()}/blacklist.txt`, { encoding: "utf-8" })
     .toLowerCase()
     .split("\r\n");
 });
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
